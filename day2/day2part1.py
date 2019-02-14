bdc3507f99f2cf638cc8565e04ba87c4373d821d
alfrdmalr <alfrdmalr@gmail.com>
2019-02-13 22:23:43 -0500

    updated print statements to comply with python 3 syntax

--- day2/day2part1.py
+++ day2/day2part1.py
@@ -7,21 +7,21 @@ twoCount = 0
 threeCount = 0
 
 for line in input:
-	dict = {}
-	seenTwo =  False
-	seenThree = False
+    dict = {}
+    seenTwo = False
+    seenThree = False
 
-	for letter in line:
-		if letter in dict:
-			dict[letter] += 1
-		else: 
-			dict[letter] = 1
+    for letter in line:
+        if letter in dict:
+            dict[letter] += 1
+        else:
+            dict[letter] = 1
 
-	for item, count in dict.items():
-		if (count == 3) and not seenThree:
-			threeCount += 1
-			seenThree = True
-		elif count == 2 and not seenTwo:
-			twoCount += 1
-			seenTwo = True
-print twoCount * threeCount
+    for item, count in dict.items():
+        if (count == 3) and not seenThree:
+            threeCount += 1
+            seenThree = True
+        elif count == 2 and not seenTwo:
+            twoCount += 1
+            seenTwo = True
+print(twoCount * threeCount)
