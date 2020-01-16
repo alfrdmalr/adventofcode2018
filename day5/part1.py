15495ae71b5d2bd21b44c99c818abd46e4813eaf
alfrdmalr <alfrdmalr@gmail.com>
2020-01-16 12:06:45 -0500

    cleanup part1 solution

--- day5/part1.py
+++ day5/part1.py
@@ -5,34 +5,33 @@ import re
 rawInput = sys.stdin.read()
 
 polymer = rawInput.strip() #goddamnit i forgot to strip the newline
-spontaneous = True
 
-def react(pol):
-  newPol = ""
-  prevChar = ""
-  reacted = False
+def react(poly):
+  pol = poly
+  spontaneous = True
 
-  for i in range(0, len(pol)):
-    c = pol[i]
-    if prevChar == "":
-      prevChar = c
-    else: #c is uppercase
-      if prevChar.swapcase() == c:
-        # print('upper and match')
-        prevChar = ""
-        reacted = True
-      else:
-        # print('upper and nonmatch')
-        newPol += prevChar
+  while spontaneous:
+    newPol = ""
+    prevChar = ""
+    reacted = False
+
+    for i in range(0, len(pol)):
+      c = pol[i]
+      if prevChar == "":
         prevChar = c
-  newPol += prevChar
+      else: #c is uppercase
+        if prevChar.swapcase() == c:
+          # print('upper and match')
+          prevChar = ""
+          reacted = True
+        else:
+          # print('upper and nonmatch')
+          newPol += prevChar
+          prevChar = c
+    newPol += prevChar
+    pol = newPol
+    spontaneous = reacted
 
-  global polymer
-  global spontaneous
-  polymer = newPol
-  spontaneous = reacted
+  return len(pol)
 
-while spontaneous:
-  react(polymer)
-
-print(len(polymer))
+print(react(polymer))
